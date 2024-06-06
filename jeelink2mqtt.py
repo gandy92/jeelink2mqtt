ab21fac37f71e75b9982be1f47579faac6cb7af6
Alexander Herrmann <106409+dArignac@users.noreply.github.com>
2024-06-06 17:41:25 +0200

    chore: adds reading of sensor data

--- jeelink2mqtt.py
+++ jeelink2mqtt.py
@@ -19,6 +19,50 @@ class Serial:
         # wait till jeelink has settled to ensure init sequence will be received
         time.sleep(2)
         log.info("Start receiving...")
+        await asyncio.gather(self.receive())
+
+    async def receive(self):
+        while True:
+            msg = await self.reader.readuntil(b"\n")
+            msg = LaCrosse.decodeMessage(msg)
+
+
+"""
+Class for decoding the LaCrosse messages.
+Protocol: http://fredboboss.free.fr/articles/tx29.php
+"""
+
+
+class LaCrosse:
+    @staticmethod
+    def decodeMessage(message):
+        # log.debug(f"LaCrosse: Decoding message: {message}")
+        values = message.split()
+
+        if len(values) == 0:
+            # if message != b"\n":
+            log.debug(f"LaCrosse: Received empty message {message}")
+            return
+
+        if values[0].startswith(b"[LaCrosse"):
+            log.debug(f"LaCrosse: Receiver is {str(values[0])}")
+            return
+
+        if values[0] != b"OK" or values[1] != b"9":
+            log.debug(f"LaCrosse: Unknown message received {message}")
+            return
+
+        id = int(values[2])
+        batteryNew = int(values[3]) >> 7
+        # type = int(values[3]) & 0x7F
+        temperature = int(values[4]) * 256 + int(values[5])
+        temperature = (float(temperature) - 1000) / 10
+        batteryWeak = int(values[6]) >> 7
+        humidity = int(values[6]) & 0x7F
+
+        log.debug(
+            f"LaCrosse: Sensor reporting: ID {id}, Temperature {temperature}, Humidity {humidity}, Battery new {batteryNew}, Battery weak {batteryWeak}"
+        )
 
 
 if __name__ == "__main__":
