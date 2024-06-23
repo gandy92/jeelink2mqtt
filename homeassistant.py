9be3e8ba11a65f2ad99031f1ef3f070a1d6d7814
Alexander Herrmann <106409+dArignac@users.noreply.github.com>
2024-06-23 15:49:38 +0200

    feat: adds temp and humidity discovery messages

--- homeassistant.py
+++ homeassistant.py
@@ -1,4 +1,5 @@
 import logging
+import json
 from paho.mqtt.client import Client
 
 TOPIC_HASS_DISCOVERY = "homeassistant/{component}/{id}/config"
@@ -14,9 +15,48 @@ class HomeAssistantSensor:
         self.name = name
 
     def publish_hass_discovery(self):
-        # FIXME add restrain
-        # FIXME publish
-        print(TOPIC_HASS_DISCOVERY.format(component=self.name.lower(), id=self.id))
+        # FIXME separate all 4 sensor values into each a single discovery topic, see https://www.home-assistant.io/integrations/mqtt/#sensors
+        # FIXME not sure if "identifiers" is set right
+        payloads = [
+            # temperature
+            {
+                "device": {
+                    "identifiers": f"{self.name.lower()}_device",
+                    "name": self.name,
+                },
+                "unique_id": f"{self.name.lower()}_temp",
+                "device_class": "temperature",
+                "unit_of_measurement": "°C",
+                "value_template": "{{ value_json.temperature}}",
+                "state_topic": TOPIC_HASS_STATE.format(
+                    component=self.name.lower(), id=self.id
+                ),
+            },
+            # humidity
+            {
+                "device": {
+                    "identifiers": f"{self.name.lower()}_device",
+                    "name": self.name,
+                },
+                "unique_id": f"{self.name.lower()}_humi",
+                "device_class": "humidity",
+                "unit_of_measurement": "%",
+                "value_template": "{{ value_json.humidity}}",
+                "state_topic": TOPIC_HASS_STATE.format(
+                    component=self.name.lower(), id=self.id
+                ),
+            },
+        ]
+        # FIXME remove
+        print(json.dumps(payloads[0]))
+        print(json.dumps(payloads[1]))
+
+        for payload in payloads:
+            self.mqtt.publish(
+                TOPIC_HASS_DISCOVERY.format(component=self.name.lower(), id=self.id),
+                json.dumps(payload),
+                retain=True,
+            )
 
     def publish_hass_change(self):
         # self.mqtt.publish("/test", self.id)
