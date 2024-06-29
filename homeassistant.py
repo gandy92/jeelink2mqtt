24e7b08b24ba5ed45c2a82c659787f2904837427
Alexander Herrmann <106409+dArignac@users.noreply.github.com>
2024-06-29 15:06:27 +0200

    feat: sends state updates for sensors

--- homeassistant.py
+++ homeassistant.py
@@ -14,25 +14,31 @@ class HomeAssistantSensor:
         self.id = id
         self.name = name
 
+    def get_unique_id(self) -> str:
+        return f"j2m_{self.name.lower()}"
+
+    def get_unique_id_with_type(self, type: str) -> str:
+        return f"{self.get_unique_id()}_{type}"
+
     def publish_hass_discovery(self):
-        # FIXME separate all 4 sensor values into each a single discovery topic, see https://www.home-assistant.io/integrations/mqtt/#sensors
+        # the different types are separated into different discovery messages
         shared = {
             "device": {
                 "identifiers": f"{self.name.lower()}_device",
                 "name": self.name,
             },
-            "state_topic": TOPIC_HASS_STATE.format(id=self.id),
+            "state_topic": TOPIC_HASS_STATE.format(id=self.get_unique_id()),
         }
 
         # temperature
         self.mqtt.publish(
-            TOPIC_HASS_DISCOVERY.format(id=f"{self.id}_temp"),
+            TOPIC_HASS_DISCOVERY.format(id=self.get_unique_id_with_type("temp")),
             json.dumps(
                 {
-                    "unique_id": f"{self.name.lower()}_temp",
+                    "unique_id": self.get_unique_id_with_type("temp"),
                     "device_class": "temperature",
                     "unit_of_measurement": "°C",
-                    "value_template": "{{ value_json.temperature}}",
+                    "value_template": "{{ value_json.temperature }}",
                     **shared,
                 }
             ),
@@ -41,20 +47,42 @@ class HomeAssistantSensor:
 
         # humidity
         self.mqtt.publish(
-            TOPIC_HASS_DISCOVERY.format(id=f"{self.id}_humi"),
+            TOPIC_HASS_DISCOVERY.format(id=self.get_unique_id_with_type("humi")),
             json.dumps(
                 {
-                    "unique_id": f"{self.name.lower()}_humi",
+                    "unique_id": self.get_unique_id_with_type("humi"),
                     "device_class": "humidity",
                     "unit_of_measurement": "%",
-                    "value_template": "{{ value_json.humidity}}",
+                    "value_template": "{{ value_json.humidity }}",
                     **shared,
                 },
             ),
             retain=True,
         )
 
-    # FIXME implement
+        # battery
+        self.mqtt.publish(
+            TOPIC_HASS_DISCOVERY.format(id=self.get_unique_id_with_type("batt")),
+            json.dumps(
+                {
+                    "unique_id": self.get_unique_id_with_type("batt"),
+                    "device_class": "battery",
+                    "value_template": "{{ value_json.battery }}",
+                    **shared,
+                },
+            ),
+            retain=True,
+        )
+
     def publish_hass_change(self):
-        # self.mqtt.publish("/test", self.id)
-        pass
+        self.mqtt.publish(
+            TOPIC_HASS_STATE.format(id=self.get_unique_id()),
+            json.dumps(
+                {
+                    "temperature": self.temperature,
+                    "humidity": self.humidity,
+                    "battery": 20 if self.batteryWeak else 100,
+                }
+            ),
+            retain=False,
+        )
